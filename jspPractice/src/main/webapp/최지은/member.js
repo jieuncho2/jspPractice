cd776d4bc84bef5eef2f70d9b739dbddee5fc0ab
jieuncho2 <jieunchoi.developer@gmail.com>
2022-09-22 12:00:17 +0900

    과제

--- jspPractice/src/main/webapp/최지은/member.js
+++ jspPractice/src/main/webapp/최지은/member.js
@@ -3,10 +3,11 @@
  */
  document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('input[type=submit]');
-    const id = document.querySelector('input[type=text]');
-    const password = document.querySelector('input[type=password]:nth-child(1)');
-    const password2 = document.querySelector('input[type=password]:nth-child(2)');
+    
     button.addEventListener('click', (event) => {
-        
-    })
+        const id = document.querySelector('input[type=text]:nth-of-type(1)');
+	    const password = document.querySelector('input[type=password]:nth-of-type(1)');
+	    const password2 = document.querySelector('input[type=password]:nth-of-type(2)');
+	    const name = document.querySelector('input[type=text]:nth-of-type(2)');
+    });
 });
