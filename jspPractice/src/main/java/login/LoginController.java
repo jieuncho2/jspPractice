e45245df07e2002ea2e3d11ac03860803809120d
jieuncho2 <jieunchoi.developer@gmail.com>
2022-10-03 23:05:28 +0900

    추가

--- jspPractice/src/main/java/login/LoginController.java
+++ jspPractice/src/main/java/login/LoginController.java
@@ -21,6 +21,11 @@ public class LoginController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		/*
+		1. request 받은 값을 처리
+		2. 모델 클래스에 값을 저장 후 로그인 인증 여부 확인
+		3. 인증 여부에 따라 다른 페이지로 이동
+		*/
 		
 		resp.setContentType("text/html; charset-utf-8");
 		
@@ -34,10 +39,10 @@ public class LoginController extends HttpServlet {
 		
 		boolean status = bean.validate();
 		
-		if(status) {
+		if(status) { // 로그인 성공한 경우 페이지 이동
 			RequestDispatcher dispatcher = req.getRequestDispatcher("mvc_success.jsp");
 			dispatcher.forward(req, resp);
-		} else {
+		} else { // 로그인 실패한 경우 페이지 이동
 			RequestDispatcher dispatcher = req.getRequestDispatcher("mvc_error.jsp");
 			dispatcher.forward(req, resp);
 		}
