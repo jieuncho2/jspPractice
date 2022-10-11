cb6a20c76eb5d0d78635de9205e5d8afb1e51fd3
jieuncho2 <jieunchoi.developer@gmail.com>
2022-10-12 08:50:29 +0900

    test

--- jspPractice/src/main/java/board/BoardController.java
+++ jspPractice/src/main/java/board/BoardController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class BoardController extends HttpServlet {
-	private static final int LISTCOUNT = 5;
+	private static final int LISTCOUNT = 5; 
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
